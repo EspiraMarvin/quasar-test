cf1864216d3891771604e6d98fb329747c289b5f
Marvin Espira <espiramarvin@gmail.com>
2021-04-26 21:56:04 +0300

    regex to check whitespaces in input fields

--- src/mixins/commonMixins.js
+++ src/mixins/commonMixins.js
@@ -2,6 +2,30 @@ const commonMixins = {
   // notification plugin for messages
 
   methods: {
+    // email validation
+    isValidEmail (val) {
+      const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
+      return emailPattern.test(val) || 'Invalid email'
+    },
+
+    // function generates random date between two dates
+    randomDate (start, end) {
+      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+    },
+
+    // check for white spaces (empty fields) in input fields
+    hasWhiteSpacesOnly (val) {
+      return val.replace(/\s/g, '').length || 'field is empty'
+    },
+    notify (message, type) {
+      this.$q.notify({
+        message: message,
+        position: 'top',
+        color: type,
+        icon: 'announcement'
+      })
+    },
+
     alert () {
       this.$q.dialog({
         title: 'Alert',
@@ -49,20 +73,6 @@ const commonMixins = {
       }).onDismiss(() => {
         // console.log('I am triggered on both OK and Cancel')
       })
-    },
-
-    notify (message, type) {
-      this.$q.notify({
-        message: message,
-        position: 'top',
-        color: type,
-        icon: 'announcement'
-      })
-    },
-    // email validation
-    isValidEmail (val) {
-      const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
-      return emailPattern.test(val) || 'Invalid email'
     }
   }
 }
