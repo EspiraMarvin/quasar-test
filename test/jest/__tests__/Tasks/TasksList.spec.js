2e770c6b7471faf754a51685e9eb92f8949c2e79
Marvin Espira <espiramarvin@gmail.com>
2021-05-07 11:26:38 +0300

    tests

--- test/jest/__tests__/Tasks/TasksList.spec.js
+++ test/jest/__tests__/Tasks/TasksList.spec.js
@@ -1,17 +1,29 @@
-// import { mount } from '@vue/test-utils'
-// // import Dashboard from 'pages/Dashboard'
-// import TasksList from '@/dashboard/Task/TasksList'
-//
-// describe('Dashboard Component Unit Tests:', () => {
-//   test('is a vue instance', () => {
-//     const wrapper = mount(TasksList, {
-//       propsData: {
-//         task: {
-//           title: 'Send benefit review by Sunday',
-//           tag: 'Reminder'
-//         }
-//       }
-//     })
-//     expect(wrapper.isVueInstance()).toBeTruthy()
-//   })
-// })
+import { mount } from '@vue/test-utils'
+import TasksList from 'components/dashboard/Task/TasksList.vue'
+
+describe('TasksList Component Unit Tests:', () => {
+  test('is a vue instance', () => {
+    const wrapper = mount(TasksList, {
+      propsData: {
+        task: {
+          title: 'Send benefit review by Sunday',
+          tag: 'Reminder'
+        }
+      }
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('renders the task title', () => {
+    const taskTitle = 'Send benefit review by Sunday'
+    const wrapper = mount(TasksList, {
+      propsData: {
+        task: {
+          title: taskTitle,
+          tag: 'Reminder'
+        }
+      }
+    })
+    expect(wrapper.html()).toContain(taskTitle)
+  })
+})
