87b3cdf1712d08159339241468ed35c3a908ea37
EspiraMarvin <espiramarvin@gmail.com>
2021-07-05 15:42:31 +0300

    updated settings pages

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,14 +1,42 @@
 <template>
     <div class="flex flex-center">
-      <Cube />
+        <div class="row no-wrap q-pa-md q-mt-xl">
+          <div class="column">
+            <div class="text-h6 q-mb-md">Settings</div>
+            <q-toggle v-model="mobileData" label="Use Mobile Data" />
+            <q-toggle v-model="bluetooth" label="Bluetooth" />
+          </div>
+
+          <q-separator vertical inset class="q-mx-lg" />
+
+          <div class="column items-center">
+            <q-avatar size="72px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+
+            <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+            <q-btn
+              color="primary"
+              label="Logout"
+              push
+              size="sm"
+              v-close-popup
+            />
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
-import Cube from '../components/setting/FancyCubeAnimation'
 export default {
   name: 'Settings',
-  components: { Cube }
+  data () {
+    return {
+      mobileData: true,
+      bluetooth: false
+    }
+  }
 }
 </script>
 
